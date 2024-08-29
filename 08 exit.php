eff6330d6e707fac1ebfd0b4fab491e8524f24be
aliyanovita <aliyaksp3@gmail.com>
2024-08-29 12:26:31 +0700

    08 exit.php

---
+++ 08 exit.php
@@ -0,0 +1,10 @@
+<?php
+echo 'Ini akan tampil di browser.';
+exit('Proses telah selesai.') ;;
+echo 'Ini tidak akan tampil di browser.';
+
+// jika terjadi error
+exit(1);
+
+//jika tidak ada error
+exit(0);
