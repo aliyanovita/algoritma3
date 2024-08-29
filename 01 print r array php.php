b7922e0d61ec36500b12c14619d03b7fcc801bfd
aliyanovita <aliyaksp3@gmail.com>
2024-08-29 13:53:09 +0700

    print array.php

--- 01 print r array php.php
+++ 01 print r array php.php
@@ -2,7 +2,7 @@
 $nama = "Budi";
 print_r($nama);
 print "<p>";
-$buah = array("apel", "pisang","ceri","mangga");
+$buah = array("apel", "pisang", "ceri", "mangga");
 print_r ($buah);
 print "<p>";
 printf ($buah [2]);
