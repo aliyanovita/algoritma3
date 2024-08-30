ebc30b96d1d19c7a9582ea1555076b67681fd288
aliyanovita <aliyaksp3@gmail.com>
2024-08-30 10:52:29 +0700

    22 percabangan if.php

---
+++ 22 percabangan if.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$total_belanja = 150000;
+
+if($total_belanja > 100000){
+   echo "karena belanjaan anda sudah melebihi $total_belanja , maka Anda akan dapat DISKON 75%!";
+}
+
+$siswa = "rajin";
+if($siswa = "rajin"){
+    echo "<hr>jika rajin akan NAIK KELAS!";
+}
+echo"<p><strong>By:Aliya novita";
