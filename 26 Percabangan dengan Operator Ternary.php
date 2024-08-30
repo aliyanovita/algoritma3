23427ff2838b4cb085d8a6a513d687d379ae4fd5
aliyanovita <aliyaksp3@gmail.com>
2024-08-30 11:10:09 +0700

    26 Percabangan dengan Operator Ternary.php

---
+++ 26 Percabangan dengan Operator Ternary.php
@@ -0,0 +1,8 @@
+<?php
+
+$suka = false;
+$suka ? print "Saya suka kamu<hr>": print "Mohon Maaf saya udah ada yang punya! <hr>";
+
+$suka = true;
+echo $suka ? "Saya suka kamu<hr>": "Mohon Maaf saya udah ada yang punya! <hr>";
+echo "<p><strong>By:Aliya novita";
