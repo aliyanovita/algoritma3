ed6a21aba63989eff54d8b3b0b630f9e3cadc361
aliyanovita <aliyaksp3@gmail.com>
2024-08-30 10:57:04 +0700

    23 percabangan ifelse.php

---
+++ 23 percabangan ifelse.php
@@ -0,0 +1,21 @@
+<?php
+
+$umur = 13;
+
+if ($umur < 18 ){
+    echo "<hr>Kamu tidak boleh membuka situs ini! umur < 13";
+} else {
+    echo "<hr>Selamat datang di website kami! umur < 13";
+}
+
+
+
+
+$umur = 19;
+
+if ($umur < 18 ){
+    echo "<hr>Kamu tidak boleh membuka situs ini! umur < 19";
+} else {
+    echo "<hr>Selamat datang di website kami! umur < 19";
+}
+echo "<p><strong>By:Aliya novita";
