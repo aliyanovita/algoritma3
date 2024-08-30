c2a00bf1d283c3a9bda9be78b97408774ab379b5
aliyanovita <aliyaksp3@gmail.com>
2024-08-30 10:26:59 +0700

    17 operator ternary.php

---
+++ 17 operator ternary.php
@@ -0,0 +1,18 @@
+<?php
+
+$suka = true;
+
+// menggunakan operator ternary
+$jawab = $suka ? "iya": "tidak";
+
+// menampilkan jawaban
+echo $jawab;
+echo "<br>";
+var_dump ($jawab);
+echo "<hr>";
+
+echo $suka;
+echo "<br>";
+var_dump ($suka);
+echo "<hr>";
+echo "<p><strong>By: Aliya novita";
