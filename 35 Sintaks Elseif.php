be9735a2de03d157c32cbe9876f087778d6b54d6
aliyanovita <aliyaksp3@gmail.com>
2024-08-31 10:08:06 +0700

    35 Sintaks Elseif.php

---
+++ 35 Sintaks Elseif.php
@@ -0,0 +1,9 @@
+<?php
+if (kondisi1) {
+    // kode dieksekusi jika kondisi1 benar
+} elseif (kondisi2) {
+    // kode dieksekusi jika kondisi1 salah dan kondisi2 benar
+} else {
+    // kode dieksekusi jika kondisi1 dan kondisi2 salah
+}
+echo "<p><strong>By:Aliya novita";
